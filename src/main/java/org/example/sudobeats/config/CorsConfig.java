86e33225c8e49d2165746176a1a2b9717ec6c879
snbaay <samatovice@gmail.com>
2026-05-11 20:40:39 +0500

    .

--- src/main/java/org/example/sudobeats/config/CorsConfig.java
+++ src/main/java/org/example/sudobeats/config/CorsConfig.java
@@ -37,7 +37,7 @@ public class CorsConfig {
         config.setMaxAge(3600L);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/api/**", config);
+        source.registerCorsConfiguration("/**", config);
 
         return new CorsFilter(source);
     }
