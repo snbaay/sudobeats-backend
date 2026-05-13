7818fb6094b8043e032d208c004ed01bd3ae9cce
snbaay <samatovice@gmail.com>
2026-05-13 23:00:25 +0500

    Add username lookup for auth login

--- src/main/java/org/example/sudobeats/repository/UserRepository.java
+++ src/main/java/org/example/sudobeats/repository/UserRepository.java
@@ -18,6 +18,7 @@ public interface UserRepository extends JpaRepository<User, UUID> {
 
     boolean existsByEmail(String email);
     Optional<User> findByEmailIgnoreCase(String email);
+    Optional<User> findByUsernameIgnoreCase(String username);
     boolean existsByEmailIgnoreCase(String email);
     boolean existsByUsernameIgnoreCase(String username);
 
